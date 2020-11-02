a5dd4b892a2168c3392df615587d5e4f04c98dd6
Gwladys <gwladys.pinel54@gmail.com>
2020-11-02 21:56:39 +0100

    add country page

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,18 @@
 const url = "https://restcountries.eu/rest/v2/all"
 const countryCardContainer = document.querySelector(".country-card-container")
+const searchContainer = document.querySelector(".search-container")
+const countryInfoContainer = document.querySelector(".country-info-container")
+const back = document.querySelector("[data-action='back']")
 
 showAll().then(countResults)
+
+
+back.addEventListener('click', function() {
+    countryCardContainer.classList.remove('hidden')
+    searchContainer.classList.remove('hidden')
+    countryInfoContainer.classList.add('hidden')
+    document.querySelector(".country-info").innerHTML = ""
+})
 
 function countries() {
     return fetch(url).then(res => res.json())
@@ -11,28 +22,39 @@ function countries() {
 }
 
 
-
 async function showAll() {
-
     let data = await countries().then((content) => {
         return content
         }
     )
-
     for (let i = 0; i < data.length;i++) {
         createCountryCard(data[i])
     }
+    getInfo(data)
+}
 
-  /*  console.log(data[0].alpha2Code);
-    let borders = "bordures : " + data[0].borders[0]
-    for (let i = 1; i < data[0].borders.length;i++) {
-        borders = borders + ', ' + data[0].borders[i]
+
+function getInfo(data) {
+    const countries = document.querySelectorAll('.country')
+    for (let i = 0; i < countries.length;i++) {
+        countries[i].addEventListener('click', function() {
+            let countryName = this.getElementsByTagName('h2')[0].innerText
+
+            for (let j = 0; j < data.length; j++) {
+                if (data[i].name === countryName) {
+                    createCountryInfo(data[i], data)
+                    break;
+                }
+            }
+
+            countryCardContainer.classList.add('hidden')
+            searchContainer.classList.add('hidden')
+            countryInfoContainer.classList.remove('hidden')
+        })
     }
-    console.log(borders)*/
 }
 
 function createCountryCard(content) {
-   // console.log(content)
     if (!content.region) {
         content.region = "undefined"
     }
@@ -48,6 +70,52 @@ function createCountryCard(content) {
         "            </div>"
 
     countryCardContainer.insertAdjacentHTML('beforeend', div)
+
+}
+
+function createCountryInfo(country, countries) {
+    let countryLanguage = ""
+    country.altSpellings.forEach(language => countryLanguage = countryLanguage + ", " + language)
+
+    let countryBorders = ""
+    for (let i = 0; i < country.borders.length; i++) {
+        for (let j = 0; j < countries.length; j++) {
+            if (countries[j].alpha3Code === country.borders[i]) {
+                countryBorders = countryBorders + "<span class='bg-blue-700 py-2 px-6 mx-3' data-countryBorders=" + countries[j].name + ">" + countries[j].name + "</span>"
+                break;
+            }
+        }
+    }
+
+    let div = "<img class='w-1/2' src='" + country.flag + "' alt=\"\">\n" +
+        "                <div>\n" +
+        "                    <h1>" + country.name + "</h1>\n" +
+        "                    <div>\n" +
+        "                        <p>Native name : " + country.name + "</p>\n" +
+        "                        <p>Population : " + country.population + "</p>\n" +
+        "                        <p>Region : " + country.region + "</p>\n" +
+        "                        <p>Sub Region : " + country.subregion + "</p>\n" +
+        "                        <p>Capital : " + country.capital + "</p>\n" +
+        "                        <p>Top Level Domain : " + country.name + "</p>\n" +
+        "                        <p>Currencies : " + country.name + "</p>\n" +
+        "                        <p>Languages : " + countryLanguage + "</p>\n" +
+        "                    </div>\n" +
+        "                    <p>Border countries : " + countryBorders + "</p>\n" +
+        "                </div>"
+    document.querySelector(".country-info").insertAdjacentHTML('beforeend', div)
+
+    document.querySelectorAll("[data-countryBorders]").forEach(borders => borders.addEventListener('click', function() {
+            for (let j = 0; j < countries.length; j++) {
+                if (borders.innerText.toLowerCase() === countries[j].name.toLowerCase()) {
+                    console.log(document.querySelector(".country-info"))
+                    document.querySelector(".country-info").innerHTML = ""
+                    createCountryInfo(countries[j], countries)
+                }
+            }
+        }
+    ))
+
+
 }
 
 async function countResults() {
@@ -57,4 +125,3 @@ async function countResults() {
     )
     showResults.innerText = shownCountries.length
 }
-
