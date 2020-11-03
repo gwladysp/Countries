c3cf19d6fbf19d5fc29f6e5ee7c07ff906832160
Gwladys <gwladys.pinel54@gmail.com>
2020-11-03 22:36:42 +0100

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    backgroundColor: ['dark', 'dark-hover', 'dark-group-hover', 'dark-even', 'dark-odd'],
+    borderColor: ['dark', 'dark-disabled', 'dark-focus', 'dark-focus-within'],
+    textColor: ['dark', 'dark-hover', 'dark-active', 'dark-placeholder']
+  },
+  plugins: [require('tailwindcss-dark-mode')()],
+}
